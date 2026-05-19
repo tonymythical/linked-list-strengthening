141547a4c8def55007cf88cdfd3de99ec9ff23c8
tonymythical <174657016+tonymythical@users.noreply.github.com>
2026-05-18 22:55:31 -0700

    worked on min

--- src/Exercises.java
+++ src/Exercises.java
@@ -76,7 +76,18 @@ public class Exercises {
      * @return the minimum value in the list 
      */
     public static int min(ListNode head) {
-        return -1;
+        if (head == null) {
+            return Integer.MAX_VALUE;
+        }
+        int minValue = head.data;
+        ListNode current = head.next;
+        while (current != null) {
+            if (current.data < minValue) {
+                minValue = current.data;
+            }
+            current = current.next;
+        }
+        return minValue;
     }
 
     /**
