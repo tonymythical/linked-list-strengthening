d3f0ee91bcce5556333e7db0c84bb914fb408f38
tonymythical <174657016+tonymythical@users.noreply.github.com>
2026-05-18 22:59:53 -0700

    done with isdoubled

--- src/Exercises.java
+++ src/Exercises.java
@@ -147,7 +147,17 @@ public class Exercises {
      * @return whether the values in bigList are twice the values in smallList
      */
     public static boolean isDoubled(ListNode smallList, ListNode bigList) {
-        return false;
+        ListNode currentSmall = smallList;
+        ListNode currentBig = bigList;
+        
+        while (currentSmall != null && currentBig != null) {
+            if (currentBig.data != currentSmall.data * 2) {
+                return false;
+            }
+            currentSmall = currentSmall.next;
+            currentBig = currentBig.next;
+        }
+        return currentSmall == null && currentBig == null;
     }
 
     /**
